009ce66e31492c02965b5f2bc1b4f404289112e8
Christopher Stock <email@christopherstock.de>
2018-10-05 18:19:18 +0200

    Completed lesson 7: exception handling.

--- src/7_exception_handling.py
+++ src/7_exception_handling.py
@@ -8,7 +8,29 @@ except ZeroDivisionError:
     print('Division by zero error caught')
 except ArithmeticError:
     print('ArithmeticError error caught')
-except Exception:
+except (Exception):
     print('Exception caught')
+except (IndexError, ArithmeticError):
+    print('One of the specified exceptions occurred')
+except:
+    print('This is a "bare except"')
 finally:
     print('Finally block being invoked')
+
+# my list
+my_list = [1, 2, 3, 4, 5]
+try:
+    my_list[6]
+except IndexError:
+    print("The index '6' is not in the list")
+
+# no exception
+print("Trying no exception:")
+try:
+    myTest2 = 0 / 1
+except ZeroDivisionError:
+    print('Division by zero error caught')
+else:
+    print('Else branch being passed!')
+finally:
+    print('Finally branch being passed!')
